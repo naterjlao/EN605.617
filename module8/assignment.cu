ff6b2723e678439dc0571037b25a4694518ad2f1
nlao <nlao@memes.memes>
2023-03-19 21:56:18 -0400

    random fix

--- module8/assignment.cu
+++ module8/assignment.cu
@@ -6,13 +6,18 @@
 #define SKIP_CPU 0
 
 __host__ void printSquareMatrix(const int matlength, float *matrix);
-__host__ void cpu_rand_matrix(const int size, float *buffer);
-__host__ void gpu_rand_matrix(const int totalThreads, const int numBlocks, const int blockSize, float *buffer);
+__host__ void identityMatrix(const int matlength, float *matrix);
+__host__ float cpu_rand_matrix(const size_t size,
+	float *buffer);
+__host__ float gpu_rand_matrix(const int totalThreads,
+	const int numBlocks,
+	const int blockSize,
+	float *buffer);
 
 int main(int argc, char **argv)
 {
 	// read command line arguments
-	int matlength = (1 << 19);
+	int matlength = (1 << 10);
 	int blockSize = 256;
 
 	if (argc >= 2)
@@ -26,7 +31,7 @@ int main(int argc, char **argv)
 
 	// Use a square matrix of matlength x matlength
 	// The area of the matrix is totalThreads
-	int totalThreads = 2 * matlength;
+	int totalThreads = matlength * matlength;
 	int numBlocks = totalThreads / blockSize;
 
 	// validate command line arguments
@@ -39,33 +44,24 @@ int main(int argc, char **argv)
 		printf("The total number of threads will be rounded up to %d\n", totalThreads);
 	}
 
-	// Setup Stop Watch
-	StopWatchInterface *stopwatch = new StopWatchLinux();
+	// Allocate Matrices
+	float *matrix_buffer = (float *)malloc(sizeof(float) * totalThreads);
+	float *matrix_identy = (float *)malloc(sizeof(float) * totalThreads);
 
-	// Allocate Host Buffer
-	float *host_buffer = (float *)malloc(sizeof(float) * totalThreads);
+	printf("%d, ", totalThreads);
 
 #if !(SKIP_CPU)
 	// CPU - Random Number Generation
-	stopwatch->reset();
-	stopwatch->start();
-	cpu_rand_matrix(totalThreads, host_buffer);
-	stopwatch->stop();
-	printf("%f,\n",stopwatch->getTime());
+	printf("%f, ",cpu_rand_matrix(totalThreads, matrix_buffer));
 #endif
-	//printSquareMatrix(matlength, host_buffer);
 
 	// GPU - Random Number Generation
-	stopwatch->reset();
-	stopwatch->start();
-	gpu_rand_matrix(totalThreads, numBlocks, blockSize, host_buffer);
-	stopwatch->stop();
-	printf("%f,\n",stopwatch->getTime());
+	printf("%f,\n",gpu_rand_matrix(totalThreads, numBlocks, blockSize, matrix_buffer));
 
-	//printSquareMatrix(matlength, host_buffer);
+	//identityMatrix(matlength, matrix_identy);
+	//printSquareMatrix(matlength, matrix_buffer);
 
-	delete stopwatch;
-	free(host_buffer);
+	free(matrix_buffer);
 }
 
 __host__ void printSquareMatrix(const int matlength, float *matrix)
@@ -80,19 +76,38 @@ __host__ void printSquareMatrix(const int matlength, float *matrix)
 	}
 }
 
+__host__ void identityMatrix(const int matlength, float *matrix)
+{
+	for (size_t d = 0; d < matlength; d++)
+	{
+		*(matrix + ((d * matlength) + d)) = 1.0;
+	}
+}
+
 //-----------------------------------------------------------------------------
 /// @brief Sets up the working buffer by generating random floats using c++ std lib.
 /// @param size Length of the buffer
 /// @param buffer pointer to local host buffer
 /// @return None; buffer is modified
 //-----------------------------------------------------------------------------
-__host__ void cpu_rand_matrix(const int size, float *buffer)
+__host__ float cpu_rand_matrix(const size_t size,
+	float *buffer)
 {
-	std::srand(420);
-	for (int i = 0; i < size; i++)
+	StopWatchInterface *stopwatch = new StopWatchLinux();
+	srand(420);
+
+	stopwatch->reset();
+	stopwatch->start();
+	for (size_t i = 0; i < size; i++)
 	{
-		buffer[i] = (float) (std::rand() / ((float) RAND_MAX));
+		buffer[i] = (float) (rand() / ((float) RAND_MAX));
 	}
+	stopwatch->stop();
+	
+	// Measure time - note that this is measured in the CPU and is in milliseconds
+	float timer = stopwatch->getTime();
+	delete stopwatch;
+	return timer;
 }
 
 //-----------------------------------------------------------------------------
@@ -103,7 +118,10 @@ __host__ void cpu_rand_matrix(const int size, float *buffer)
 /// @param buffer pointer to local host buffer
 /// @return None; buffer is modified
 //-----------------------------------------------------------------------------
-__host__ void gpu_rand_matrix(const int totalThreads, const int numBlocks, const int blockSize, float *buffer)
+__host__ float gpu_rand_matrix(const int totalThreads,
+	const int numBlocks,
+	const int blockSize,
+	float *buffer)
 {
 	curandState *r_state;
 	cudaMalloc(&r_state, totalThreads * sizeof(curandState));
@@ -112,11 +130,25 @@ __host__ void gpu_rand_matrix(const int totalThreads, const int numBlocks, const
 	float *dev_buffer;
 	cudaMalloc(&dev_buffer, totalThreads * sizeof(float));
 
+	// Setup Timer
+	cudaEvent_t start;
+	cudaEvent_t end;
+	float timer;
+	cudaEventCreate(&start);
+	cudaEventCreate(&end);
+
 	// Create random F32s
+	cudaEventRecord(start);
 	populate_random_floats<<<numBlocks, blockSize>>>(dev_buffer, r_state);
 	cudaDeviceSynchronize();
-
+	cudaEventRecord(end);
+	
+	// Copy To Host
 	cudaMemcpy(buffer, dev_buffer, totalThreads * sizeof(float), cudaMemcpyDeviceToHost);
 	cudaFree(r_state);
 	cudaFree(dev_buffer);
+	
+	// Measure Time - note that this measured from CUDA and is in milliseconds
+	cudaEventElapsedTime(&timer, start, end);
+	return timer;
 }
