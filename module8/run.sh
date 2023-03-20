4d54a54240bda25d064065fc571ed2d6879ea60e
nlao <nlao@memes.memes>
2023-03-19 22:58:03 -0400

    update run

---
+++ module8/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Usage:
+# ./run.sh
+make c
+make main
+rm -rvf results.csv
+touch results.csv
+echo -n "matrix_size,cpu_rand,gpu_rand,cpu_mult,gpu_mult" >> results.csv
+echo "" >> results.csv
+
+# 2^210
+maxDim=1024
+threads=2
+while [ $threads -le $maxThreads ]
+do
+    echo "EXECUTING $threads THREADS"
+    echo -n "$threads," >> results.csv
+    ./main $threads >> results.csv
+    threads=$(($threads * 2))
+done
