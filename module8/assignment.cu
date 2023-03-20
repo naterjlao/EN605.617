530584401df9248dc2a06716b4d453a615b679e6
nlao <nlao@memes.memes>
2023-03-19 22:53:49 -0400

    complete

--- module8/assignment.cu
+++ module8/assignment.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cublas.h>
 
 #include "helpers.cuh"
 #include "helper_timer.h"
@@ -13,7 +14,14 @@ __host__ float gpu_rand_matrix(const int totalThreads,
 							   const int numBlocks,
 							   const int blockSize,
 							   float *buffer);
-__host__ void cpu_mat_mult(const int matlength, float *A, float *B, float *C);
+__host__ float cpu_mat_mult(const int matlength,
+							const float *A,
+							const float *B,
+							float *C);
+__host__ float gpu_mat_mult(const int matlength,
+							const float *A,
+							const float *B,
+							float *C);
 
 int main(int argc, char **argv)
 {
@@ -58,18 +66,22 @@ int main(int argc, char **argv)
 #endif
 
 	// GPU - Random Number Generation
-	printf("%f,\n", gpu_rand_matrix(totalThreads, numBlocks, blockSize, matrix_input));
+	printf("%f, ", gpu_rand_matrix(totalThreads, numBlocks, blockSize, matrix_input));
 
 	// Identity Matrix
 	identityMatrix(matlength, matrix_identy);
-	printSquareMatrix(matlength, matrix_input);
+	//printSquareMatrix(matlength, matrix_input);
 
 	// Result Matrix
 	memset(matrix_output, 0, sizeof(float) * totalThreads);
 
-	cpu_mat_mult(matlength, matrix_identy, matrix_input, matrix_output);
+	// CPU - Matrix Multiply
+#if !(SKIP_CPU)
+	printf("%f, ", cpu_mat_mult(matlength, matrix_identy, matrix_input, matrix_output));
+#endif
 
-	printSquareMatrix(matlength, matrix_output);
+	printf("%f\n", gpu_mat_mult(matlength, matrix_identy, matrix_input, matrix_output));
+	//printSquareMatrix(matlength, matrix_output);
 
 	free(matrix_input);
 	free(matrix_identy);
@@ -105,9 +117,9 @@ __host__ void identityMatrix(const int matlength, float *matrix)
 __host__ float cpu_rand_matrix(const size_t size,
 							   float *buffer)
 {
-	StopWatchInterface *stopwatch = new StopWatchLinux();
 	srand(420);
 
+	StopWatchInterface *stopwatch = new StopWatchLinux();
 	stopwatch->reset();
 	stopwatch->start();
 	for (size_t i = 0; i < size; i++)
@@ -170,11 +182,16 @@ __host__ float gpu_rand_matrix(const int totalThreads,
 /// @param A Left Matrix
 /// @param B Right Matrix
 /// @param C Output Matrix
-/// @return
-__host__ void cpu_mat_mult(const int matlength, float *A, float *B, float *C)
+/// @return Measured Computation Time (Milliseconds)
+__host__ float cpu_mat_mult(const int matlength,
+							const float *A,
+							const float *B,
+							float *C)
 {
-	float p;
 	float sum;
+	StopWatchInterface *stopwatch = new StopWatchLinux();
+	stopwatch->reset();
+	stopwatch->start();
 	for (int idx = 0; idx < matlength; idx++)
 	{
 		for (int jdx = 0; jdx < matlength; jdx++)
@@ -187,4 +204,123 @@ __host__ void cpu_mat_mult(const int matlength, float *A, float *B, float *C)
 			C[idx + (matlength * jdx)] = sum;
 		}
 	}
+	stopwatch->stop();
+
+	// Measure time - note that this is measured in the CPU and is in milliseconds
+	float timer = stopwatch->getTime();
+	delete stopwatch;
+	return timer;
+}
+
+/// @brief GPU Square Matrix Multiplication
+/// @param matlength Single Dimension of the input Matrices
+/// @param A Left Matrix
+/// @param B Right Matrix
+/// @param C Output Matrix
+/// @return Measured Computation Time (Milliseconds)
+__host__ float gpu_mat_mult(const int matlength,
+							const float *A,
+							const float *B,
+							float *C)
+{
+	cublasStatus status;
+	cublasInit();
+	const int matdim = matlength * matlength;
+	float *AA;
+	float *BB;
+	float *CC;
+
+	/*ALLOCATE ON THE DEVICE*/
+	status = cublasAlloc(matdim, sizeof(float), (void **)&AA);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! device memory allocation error (A)\n");
+		return EXIT_FAILURE;
+	}
+
+	status = cublasAlloc(matdim, sizeof(float), (void **)&BB);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! device memory allocation error (A)\n");
+		return EXIT_FAILURE;
+	}
+
+	status = cublasAlloc(matdim, sizeof(float), (void **)&CC);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! device memory allocation error (A)\n");
+		return EXIT_FAILURE;
+	}
+
+	/*SET MATRIX*/
+	status = cublasSetMatrix(matlength, matlength, sizeof(float), A, matlength, AA, matlength);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! device memory allocation error (A)\n");
+		return EXIT_FAILURE;
+	}
+
+	status = cublasSetMatrix(matlength, matlength, sizeof(float), B, matlength, BB, matlength);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! device memory allocation error (A)\n");
+		return EXIT_FAILURE;
+	}
+
+	// Setup Timer
+	cudaEvent_t start;
+	cudaEvent_t end;
+	float timer;
+	cudaEventCreate(&start);
+	cudaEventCreate(&end);
+
+	/*KERNEL*/
+	cudaEventRecord(start);
+	cublasSgemm('n', 'n', matlength, matlength, matlength, 1, AA, matlength, BB, matlength, 0, CC, matlength);
+	cudaEventRecord(end);
+
+	status = cublasGetError();
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! kernel execution error.\n");
+		return EXIT_FAILURE;
+	}
+	cublasGetMatrix(matlength, matlength, sizeof(float), CC, matlength, C, matlength);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! device read error (A)\n");
+		return EXIT_FAILURE;
+	}
+
+	/* PERFORMANCE OUTPUT*/
+	status = cublasFree(AA);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! memory free error (A)\n");
+		return EXIT_FAILURE;
+	}
+	status = cublasFree(BB);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! memory free error (B)\n");
+		return EXIT_FAILURE;
+	}
+	status = cublasFree(CC);
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! memory free error (C)\n");
+		return EXIT_FAILURE;
+	}
+
+	/* Shutdown */
+	status = cublasShutdown();
+	if (status != CUBLAS_STATUS_SUCCESS)
+	{
+		fprintf(stderr, "!!!! shutdown error (A)\n");
+		return EXIT_FAILURE;
+	}
+
+	// Measure Time - note that this measured from CUDA and is in milliseconds
+	cudaEventElapsedTime(&timer, start, end);
+	return timer;
 }
